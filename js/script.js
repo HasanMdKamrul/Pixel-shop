9a7327a532f3372486af46791182750952619860
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-18 00:37:13 +0200

    Display products section done

--- js/script.js
+++ js/script.js
@@ -41,7 +41,11 @@ const dataDisplay = async()=>{
           <h5
             class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
           >
-            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+            ${name}
+            <div class='flex gap-3'>
+                <img class='w-5' src="../heart.png" alt="" />
+                <img class='w-5' src="../recycle.png" alt="" />
+            </div>
           </h5>
         </a>
         <div class="flex items-center mt-2.5 mb-5">
@@ -112,7 +116,12 @@ const dataDisplay = async()=>{
         </div>
         <div class="flex justify-between items-center">
           <span class="text-3xl font-bold text-gray-900 dark:text-white"
-            >$599</span
+            >$${price}</span
+          >
+          <a
+            href="#"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >Details</a
           >
           <a
             href="#"
