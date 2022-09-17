550ca48d5c40027d781f6abe7a4e31e337da1d9b
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-18 01:56:11 +0200

    Add to cart data returned for display

--- js/script.js
+++ js/script.js
@@ -120,6 +120,7 @@ const dataDisplay = async()=>{
           >
           <label onclick="loadPhones('${id}')" for="my-modal" class="btn modal-button">Details</label>
           <button
+            onclick="addToCart('${id}')"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >Add to cart</
           button>
@@ -132,6 +133,27 @@ const dataDisplay = async()=>{
 
     })
 };
+// ** Add to cart functionality
+
+const products = [];
+let count;
+
+const addToCart = async(productId)=>{
+  // ** load data
+  const phones = await dataLoader();
+  // **
+  const item = phones.find(phone => productId === phone.id);
+
+  products.push(item);
+
+  count ++;
+  
+  return products;
+};
+
+// ** display add to cart
+
+
 
 // ** load individual phone details
 
@@ -170,10 +192,7 @@ const phoneDisplay = phone=>{
     <label for="my-modal" class="btn">Yay!</label>
 </div>
   
-  `
-
-
-
+  `;
 
 }
 
