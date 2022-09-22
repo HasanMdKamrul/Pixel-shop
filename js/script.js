8fde0a6dcdd7746d116bd0916244e79e9a0175d0
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-22 03:45:51 +0200

    Order confirmed added

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ const dataDisplay = async()=>{
   const phones = await dataLoader();
   // console.log(phones)
   // ** get the individual phones
-
+  
   phones.forEach(phone => {
       const {id,price,img,name} = phone;
 
@@ -249,6 +249,7 @@ savedProducts.forEach((product,index) => {
     <p id="taxprice">Tax:${tax ? tax.toFixed(2) : 0}</p>
     <p id="grandtotal">Total:${totalPriceOfProducts ? totalPriceOfProducts.toFixed(2): 0} </p>
     <button onclick="clearProducts()" class="px-3 py-2 bg-blue-500 rounded-xl text-white">Clear products</button>
+    <button onclick="order()" class="px-3 my-5 py-2 bg-blue-500 rounded-xl text-white">Order Confirm</button>
   </div>
 
   `
@@ -256,6 +257,135 @@ savedProducts.forEach((product,index) => {
 });
 productCounter();
 };
+
+// ** order display
+
+const order = ()=>{
+  const savedProducts = getLocalStorageData();
+  const cardContainer = document.getElementById('card-container');
+
+  cardContainer.textContent = ``;
+  // ** get the individual phones
+
+  // cardContainer.parentNode.innerHTML = `<h1>Thank you for your order</h1>`
+  
+  savedProducts.forEach(phone => {
+      const {id,price,img,name} = phone;
+
+      const div = document.createElement('div');
+
+      div.classList.add('w-full','max-w-sm','rounded-lg','shadow-md','dark:bg-gray-800','dark:border-gray-700')
+
+      div.innerHTML = `
+      
+      <a href="#">
+      <img
+        class="p-8 rounded-t-lg"
+        src="${img}"
+        alt="product image"
+      />
+    </a>
+    <div class="px-5 pb-5">
+      <a href="#">
+        <h5
+          class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+        >
+          ${name}
+          <div class='flex gap-3'>
+              <img class='w-5' src="../heart.png" alt="" />
+              <img class='w-5' src="../recycle.png" alt="" />
+          </div>
+        </h5>
+      </a>
+      <div class="flex items-center mt-2.5 mb-5">
+        <svg
+          aria-hidden="true"
+          class="w-5 h-5 text-yellow-300"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>First star</title>
+          <path
+            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+          ></path>
+        </svg>
+        <svg
+          aria-hidden="true"
+          class="w-5 h-5 text-yellow-300"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Second star</title>
+          <path
+            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+          ></path>
+        </svg>
+        <svg
+          aria-hidden="true"
+          class="w-5 h-5 text-yellow-300"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Third star</title>
+          <path
+            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+          ></path>
+        </svg>
+        <svg
+          aria-hidden="true"
+          class="w-5 h-5 text-yellow-300"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Fourth star</title>
+          <path
+            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+          ></path>
+        </svg>
+        <svg
+          aria-hidden="true"
+          class="w-5 h-5 text-yellow-300"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Fifth star</title>
+          <path
+            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+          ></path>
+        </svg>
+        <span
+          class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3"
+          >5.0</span
+        >
+      </div>
+      <div class="flex justify-between items-center">
+        <span class="text-3xl font-bold text-gray-900 dark:text-white"
+          >$${price}</span
+        >
+        <label onclick="loadPhones('${id}')" for="my-modal" class="btn modal-button">Details</label>
+        <button
+          onclick="addToCart('${id}')"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >Add to cart</
+        button>
+      </div>
+    </div>
+
+      `;
+
+      cardContainer.appendChild(div);
+
+      displayAddToCart();
+
+  })
+  
+
+}
 
 const getInnerText = id => parseFloat(document.getElementById(id).innerText);
 
@@ -362,7 +492,7 @@ phoneContainer.textContent = ``
 
 phoneContainer.innerHTML = `
 <div class="card w-96 bg-base-100 shadow-xl image-full">
-<><figure>
+<figure>
     <img src="${img}" alt="Shoes" />
   </figure><div class="card-body">
       <h2 class="card-title text-5xl text-cyan-600">Product: ${name}</h2>
